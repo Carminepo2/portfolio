8bcf2e1c4ba0f940bc03bdf4f74809732bce515f
Carmine <Carmineporricelli96@gmail.com>
2021-06-26 20:14:23 +0200

    fixed background blob disappearance

--- components/sections/BackgroundBlob.tsx
+++ components/sections/BackgroundBlob.tsx
@@ -5,9 +5,17 @@ interface BackgroundBlobProps {
   className?: string;
 }
 
+const colorsClasses = {
+  pink: "bg-pink-300",
+  yellow: "bg-yellow-300",
+  purple: "bg-purple-300",
+};
+
 function BackgroundBlob({ color, className }: BackgroundBlobProps) {
   return (
-    <div className={`animate-blob absolute rounded-full bg-${color}-300 bg-opacity-70 filter blur-xl ${className}`} />
+    <div
+      className={`animate-blob absolute rounded-full ${colorsClasses[color]} bg-opacity-70 filter blur-xl ${className}`}
+    />
   );
 }
 
