a677fe41ccaade6827ca3220b953e6652f133e82
Carmine <Carmineporricelli96@gmail.com>
2021-06-26 23:24:57 +0200

    save changes

--- components/sections/BackgroundBlob.tsx
+++ components/sections/BackgroundBlob.tsx
@@ -15,20 +15,20 @@ const colorsClasses = {
 
 function BackgroundBlob({ color, sizeClasses, positionClasses }: BackgroundBlobProps) {
   const { scrollYProgress } = useViewportScroll();
-  const y = useTransform(scrollYProgress, [0, 1], [0, -200]);
+  const translateY = useTransform(scrollYProgress, [0, 1], [0, -200]);
 
   return (
-    <motion.div className={`absolute ${positionClasses}`} style={{ y }}>
+    <motion.div className={`absolute ${positionClasses}`} style={{ translateY }}>
       <motion.div
         animate={{
           scale: [1, 1.2, 1.4, 1],
-          x: [0, 80, -10, 0],
-          y: [0, -30, 30, 0],
+          translateX: [0, 80, -10, 0],
+          translateY: [0, -30, 30, 0],
         }}
         transition={{
           repeat: Infinity,
           repeatType: "reverse",
-          duration: 20,
+          duration: 10,
           delay: Math.random(),
         }}
         className={`absolute rounded-full ${colorsClasses[color]} bg-opacity-70 filter blur-xl ${sizeClasses}`}
